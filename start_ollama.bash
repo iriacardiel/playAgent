6c027fdfb4a41450085172c4f8e31ab982f27bbd
iriacardielperez <iriacardielperez@gmail.com>
2025-09-12 12:10:10 +0200

    ollama route

--- start_ollama.bash
+++ start_ollama.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export OLLAMA_MODELS=/opt/.ollama/models
+export OLLAMA_MODELS=/opt/.ollama-dori/models
 export OLLAMA_KEEP_ALIVE=24h
 echo "OLLAMA_MODELS:" $OLLAMA_MODELS
 echo "OLLAMA_KEEP_ALIVE:" $OLLAMA_KEEP_ALIVE
